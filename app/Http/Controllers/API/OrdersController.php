81d019fa8eed90132fcb03cfbbb3dcf001b85eb1
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2025-01-07 04:34:06 +0300

    fix

--- app/Http/Controllers/API/OrdersController.php
+++ app/Http/Controllers/API/OrdersController.php
@@ -167,7 +167,7 @@ class OrdersController extends Controller
     }
     public function addAllItemsToCart(Request $request)
     {
-        dd($request->objects);
+        // dd($request->objects);
         try {
             $objects = json_decode($request->objects, true);
             // dd($objects);
